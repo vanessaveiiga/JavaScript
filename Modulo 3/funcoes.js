6cc7227cd891918abd5faaed50333bffe46c0328
Vanesa <vanessaveiiga@gmail.com>
2025-03-10 10:50:08 -0300

    teste

---
+++ Modulo 3/funcoes.js
@@ -0,0 +1,15 @@
+// Funções
+// Precisa chamar
+// Pode ou não receber parâmetros
+// Pode ou não retornar dados
+
+function gerarNome(){
+    const nome = 'Vanessa'
+    const sobrenome = ' Veiga'
+    const idade = 30
+
+    console.log('Nome: ', nome + sobrenome)
+    console.log('Idade: ', idade)
+}
+
+gerarNome()
